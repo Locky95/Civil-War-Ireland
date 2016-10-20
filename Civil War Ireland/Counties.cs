350fc5f25aae79098287b069095843f061a6217a
dclocky <dclocky3@gmail.com>
2016-10-20 21:04:08 +0100

    Base classes
    code added for the basic base classes of the game

---
+++ Civil War Ireland/Counties.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Civil_War_Ireland
+{
+    class Counties
+    {
+        int countyID;
+        enum Province {Leinster,Munster,Ulster,Connaught};
+        String countyName;
+
+        public Counties()
+        {
+
+        }
+        public Counties(int countyID, String countyName, Province province)
+        {
+            setCountyID(countyID);
+            setCountyName(countyName);
+            setProvince(province);
+
+        }
+
+        private void setProvince(Province province)
+        {
+            this.province = province;
+        }
+
+        private void setCountyName(string countyName)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void setCountyID(int countyID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Civil War Ireland/Gold.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data.OracleClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Civil_War_Ireland
+{
+    class Gold
+    {
+        int playerID;
+        int playerGold;
+
+        public Gold()
+        {
+
+        }
+        public Gold(int playerID,int playerGold)
+        {
+            setPlayerID(playerID);
+            setPlayerGold(playerGold);
+        }
+
+        void setPlayerID(int playerID)
+        {
+            this.playerID = playerID;
+        }
+
+        void setPlayerGold(int playerGold)
+        {
+            this.playerGold = playerGold;
+        }
+        
+        int getPlayerID()
+        {
+            return playerID;
+        }
+        int getPlayerGold()
+        {
+            return playerGold;
+        }
+
+       /* void generatePlayerGoldFromCountiesOwned()
+        {
+        the code to generate the gold from all the countys owned should be placed in here
+        }*/
+    }
+}
