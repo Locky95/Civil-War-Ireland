627eedb4d64cb8d4935fc4c3d3e4fea5e67c8447
dclocky <dclocky3@gmail.com>
2016-10-20 21:13:55 +0100

    Revert "Revert "Base classes""
    This reverts commit 4110a807fab50567d0c2b4dc5fc7aca4e68ec122.

---
+++ Civil War Ireland/DbConnect.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OracleClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    class DbConnect
+    {
+
+
+       // public const String oradb = "Data Source=Oracle;User Id = ;Password = ;";
+       // public const String oradb = "Data Source=Oracle;User Id = ;Password = ;";
+
+
+    }
+}
---
+++ Civil War Ireland/User.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Data.OracleClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Civil_War_Ireland
+{
+    class User
+    {
+        int userID;
+        String userName;
+        String userPassword;
+        String userEmail;
+
+        public User()
+        {
+        }
+        public User(int userID,String userName,String userPassword, String userEmail)
+        {
+            setUserId(userID);
+            setUserName(userName);
+            setUserPassword(userPassword);
+            setUserEmail(userEmail);
+        }
+
+        void setUserEmail(string userEmail)
+        {
+            this.userEmail = userEmail;
+        }
+
+        void setUserPassword(string userPassword)
+        {
+            this.userPassword = userPassword;
+        }
+
+        void setUserName(string userName)
+        {
+            this.userName = userName;
+        }
+
+        void setUserId(int userID)
+        {
+            this.userID = userID;
+        }
+        String getUserEmail()
+        {
+            return userEmail;
+        }
+        String getUserPassword()
+        {
+            return userPassword;
+        }
+        String getUserName()
+        {
+            return userName;
+        }
+        int getUserID()
+        {
+            return userID;
+        }
+        //public int getNextUserId()
+        //{
+        //    int NextUserId;
+        //    OracleConnection conn = new OracleConnection(DbConnect.oradb);
+
+
+        //    //Define SQL query
+        //    String strSQL = "SELECT MAX(UserId) FROM Users";
+
+        //    //Define Oracle command
+        //    OracleCommand cmd = new OracleCommand(strSQL, conn);
+
+        //    //Open DB connection
+        //    conn.Open();
+
+        //    //execute
+        //    OracleDataReader dr = cmd.ExecuteReader();
+
+        //    //read the record in dr
+        //    dr.Read();
+
+        //    if (dr.IsDBNull(0))
+        //        NextUserId = 1;
+        //    else
+        //        NextUserId = Convert.ToInt16(dr.GetValue(0)) + 1;
+
+        //    conn.Close();
+        //    return NextUserId;
+        //}
+    }
+}
