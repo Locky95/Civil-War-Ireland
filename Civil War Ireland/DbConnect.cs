86c7ceeff48d319c100063f5717e909356ad20d7
Daryn <t00182938@1XX64006A3DA7D8>
2016-11-24 16:26:25 +0000

    ADO made for the player class.

--- Civil War Ireland/DbConnect.cs
+++ Civil War Ireland/DbConnect.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Prototype
+namespace Civil_War_Ireland
 {
     class DbConnect
     {
--- Civil War Ireland/Player.cs
+++ Civil War Ireland/Player.cs
@@ -49,34 +49,6 @@ namespace Civil_War_Ireland
         {
             return playerID;
         }
-        //public int getNextPlayerId()
-        //{
-        //    int NextPlayerId;
-        //    OracleConnection conn = new OracleConnection(DbConnect.oradb);
-
-
-        //    //Define SQL query
-        //    String strSQL = "SELECT MAX(playerID) FROM Players";
-
-        //    //Define Oracle command
-        //    OracleCommand cmd = new OracleCommand(strSQL, conn);
-
-        //    //Open DB connection
-        //    conn.Open();
-
-        //    //execute
-        //    OracleDataReader dr = cmd.ExecuteReader();
-
-        //    //read the record in dr
-        //    dr.Read();
-
-        //    if (dr.IsDBNull(0))
-        //        NextPlayerId = 1;
-        //    else
-        //        NextPlayerId = Convert.ToInt16(dr.GetValue(0)) + 1;
-
-        //    conn.Close();
-        //    return NextPlayerId;
-        //}
+ 
     }
 }
